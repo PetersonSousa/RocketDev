726c8703cb4869dc6740036b8fd922aad03bde5d
PetersonSousa <peterson.soousa@outlook.com>
2024-01-16 12:01:55 -0300

    Modularizando o JS

--- IMC/script.js
+++ IMC/script.js
@@ -1,28 +1,13 @@
+import { Modal } from  './modal.js'
+
 //variaveis
 const form =  document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
 
-//Estrutura de dados do modal
-
-const Modal = {
-
-    wrapper: document.querySelector('.modal-wrapper'),
-    message:document.querySelector('.modal .title span'),
-    buttonClose:document.querySelector ('.modal button.close'),
-
-    open(){
-        Modal.wrapper.classList.add('open')
-    },
-
-    close(){
-        Modal.wrapper.classList.remove('open')
-    }
-}
-//Fim
 
 
-form.onsubmit = function (event) {
+form.onsubmit = event => {
     event.preventDefault()
 
     const weight = inputWeight.value
@@ -35,13 +20,6 @@ form.onsubmit = function (event) {
     Modal.open()
     
 }
-
-
-
- Modal.buttonClose.onclick = () =>{
-         Modal.close()   
-    }
-
 
 function IMC (weight, height){
 
